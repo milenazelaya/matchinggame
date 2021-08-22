be5e2654dcae098b478509ea104558f75541acbd
Milena Zelaya <Pc@DESKTOP-SV81GQ3>
2021-08-22 16:26:41 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace matchinggame
+{
+    public partial class Form1 : Form
+    {
+        // Utilice este objeto aleatorio para elegir iconos aleatorios para los cuadrados
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
